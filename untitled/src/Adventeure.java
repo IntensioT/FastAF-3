e62cb098eb793b055c0264da4219f82ed2fd6acb
MenterCOX <danya.yagnish@mail.ru>
2021-10-13 16:49:24 +0300

    d

---
+++ untitled/src/Adventeure.java
@@ -0,0 +1,14 @@
+public class Adventeure {
+
+
+    public static void main(String args[]) {
+
+        int i = 1;
+        int b = 3;
+        for (i = 1; i < 10; i++) {
+            i = i + b;
+            System.out.println("i = " + i);
+        }
+
+    }
+}
