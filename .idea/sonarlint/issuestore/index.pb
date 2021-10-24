f2a1e4b1a20878938d8f004db784501510de56e5
MenterCOX <danya.yagnish@mail.ru>
2021-10-24 20:06:21 +0300

    dd

--- .idea/sonarlint/issuestore/index.pb
+++
@@ -1,4 +0,0 @@
-
-:
-
-FastAF.iml,f\6\f6afadc6ea4a7aabc68c4585d45e7649ba328d25
---
+++ src/test/java/CchkenTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+
+public class CchkenTest {
+
+    @org.junit.Test
+    public void main() {
+        String test="Helloworld";
+        assertEquals("Helloworld",test);
+    }
+}
